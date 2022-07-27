454d689e871208778337a43e5a6c84522652e695
Sai Myo Myat <saimyomyat511@gmail.com>
2022-07-27 23:19:27 +0630

    adding routes

--- server.js
+++ server.js
@@ -9,17 +9,9 @@ app.use(logger)
 
 app.use(express.static(path.join(__dirname, '/public')))
 
-app.get('^/$|/index(.html)?', (req,res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"))
-})
-
-app.get('/new-page(.html)?', (req,res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"))
-})
-
-app.get('/old-page(.html)?',(req,res) =>{
-  res.redirect(301, '/new-page.html')
-})
+app.use('/' ,require('./routes/root'));
+app.use('/subdir',require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'))
 
 app.all('*', (req,res) => {
   res.status(404);
