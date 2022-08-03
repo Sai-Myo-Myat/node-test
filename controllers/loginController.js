31961ecd4ddbddc6747478dc956c5b03ba3e9b7e
Sai Myo Myat <saimyomyat511@gmail.com>
2022-08-03 23:41:48 +0630

    jwt

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,6 +4,11 @@ const userDB = {
 }
 const bcrypt = require('bcrypt')
 
+const jwt  = require('jsonwebtoken');
+const path = require('path');
+require('dotenv').config();
+const fsPromises  = require('fs').promises;
+
 const loginController =async (req,res) => {
   const {user, pwd} = req.body;
   if(!user || !pwd) return res.status(400).json({"message": "UsserName and Password are required"});
@@ -12,7 +17,27 @@ const loginController =async (req,res) => {
   if(!foundUser) return res.status(401).json({"message": "you are not authorized"}) //unauthorized
   const match = await bcrypt.compare(pwd, foundUser.pwd);
   if(match) {
-    res.status(200).json({"message": "you have logged in"})
+    const accessToken = jwt.sign(
+      {"username": foundUser.username},
+      process.env.ACCESS_SECRET_KEY,
+      {expiresIn: "30s"}
+    );
+    const refreshToken = jwt.sign(
+      {"username": foundUser.username},
+      process.env.REFRESH_SECRET_KEY,
+      {expiresIn: "1d"}
+    );
+      //saving refresh token with user
+    const otherUsers = userDB.users.filter(person => person.username !== foundUser.username);
+    const upgradeUser = { ...foundUser, refreshToken};
+    userDB.setUsers([...otherUsers, upgradeUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, '..', "model", "users.json"),
+        JSON.stringify(userDB.users)
+    )
+    //sending token to client
+    res.cookie('jwt', refreshToken, {httpOnly: true, maxAge: 24 * 60 * 60 * 1000});
+    res.json({accessToken});
   }else {
     res.status(401).end();
   }
---
+++ middleware/verifyJWT.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// const verifyJWT = (req,res,next) => {
+//   const authHeader = req.headers['authorization'];
+//   console.log("authHeader", authHeader, "objects": req.headers);
+//   if(!authHeader)
+
+// }
